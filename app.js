190686941ffb34456186e7b268f0cd859c00fb74
Hassan <hassan.reyes@gmail.com>
2016-12-28 21:21:34 +0000

    heroku integration

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var pollRoutes =require("./routes/polls");
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/voating');
+var dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/voating";
+mongoose.connect(dbUrl);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
